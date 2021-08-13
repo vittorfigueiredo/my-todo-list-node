9509b002838de250d5d25542cb141c306ab8d1c7
Vitor <vitorfigueiredo855@gmail.com>
2021-08-12 23:48:06 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = require("./src/routes");
+
+const app = express();
+const port = 3000;
+
+app.use(router);
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+module.exports = app;
